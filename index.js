a3fdf84ca30de77a0f3bfd78bc19e4e4b97c74d7
gvergnaud <gabriel.vergnaud@hetic.net>
2016-09-28 10:19:19 -0400

    Add /mute and state gestion

--- index.js
+++ index.js
@@ -9,13 +9,22 @@ const { Spinner } = require('clui')
 const playSound = require('play-sound')
 const notifier = require('node-notifier')
 const path = require('path')
+const { has } = require('lodash/fp')
 
 
 class App {
   constructor(serverUrl) {
     this.socket = io(serverUrl)
-    this.username = 'An0nYM0u5'
     this.player = playSound()
+
+    this.state = {
+      username: 'An0nYM0u5',
+      isMuted: false
+    }
+  }
+
+  setState(stateAtom) {
+    this.state = Object.assign({}, this.state, stateAtom)
   }
 
   start() {
@@ -23,7 +32,7 @@ class App {
     this.connect()
       .then(() => this.printConnectionSuccess())
       .then(() => this.login())
-      .then(username => this.username = username)
+      .then(username => this.setState({ username }))
       .then(() => this.listen())
       .then(() => this.printPrompt())
   }
@@ -44,10 +53,10 @@ class App {
           name: 'username',
           type: 'input',
           message: 'Enter your username:',
-          validate: function(value) {
+          validate: value => {
             if (value.length > 10) {
               return 'W4y to0 long...'
-            } else if (!value) {
+            } else if (!value.trim()) {
               return 'Pl34ze tYp3 y0ur Uz3rN4me'
             } else {
               return true
@@ -81,16 +90,42 @@ class App {
       .prompt([{
         name: 'message',
         type: 'input',
-        message: `${this.username}:`
+        message: `${this.state.username}:`,
+        validate: value => {
+          if (value.length > 255) {
+            return 'W4y to0 long...'
+          } else if (!value.trim()) {
+            return 'Th1s 1s 4 Mess3ngeR 4pp. TyP3 a F#ck1n\' M3ss4ge.'
+          } else {
+            return true
+          }
+        }
       }])
-      .then(({ message }) => this.emitMessage(message))
+      .then(({ message }) => this.handleMessage(message))
       .then(() => this.printPrompt())
+  }
+
+  handleMessage(message) {
+    const msg = message.trim()
+
+    const handlers = {
+      '/mute': 'toggleMute',
+    }
+
+    if (has(msg, handlers)) this[handlers[msg]](msg)
+    else this.emitMessage(msg)
+
+    return Promise.resolve()
+  }
+
+  toggleMute() {
+    this.setState({ isMuted: !this.state.isMuted })
   }
 
   emitMessage(message) {
     this.socket.emit('message', {
       content: message,
-      username: this.username
+      username: this.state.username
     })
     return Promise.resolve()
   }
@@ -100,12 +135,17 @@ class App {
   }
 
   onReceiveMessage({ username, content }) {
-    this.player.play(path.join(__dirname, 'assets', 'media', 'decay.mp3'))
-    notifier.notify({
-      title: `${username} s4y5 :`,
-      message: content,
-      icon: path.join(__dirname, 'assets', 'images', 'notif-thumbnail.png')
-    })
+
+    if (!this.state.isMuted) {
+      // this.player.play(path.join(__dirname, 'assets', 'media', 'decay.mp3'))
+      notifier.notify({
+        title: `${username} s4y5 :`,
+        message: content,
+        icon: path.join(__dirname, 'assets', 'images', 'notif-thumbnail.png'),
+        sound: true,
+      })
+    }
+
     process.stdout.write("\r\x1b[K")
     console.log(chalk.green('?'), chalk.white.bold(`${username}: `) + chalk.cyan(content))
   }
